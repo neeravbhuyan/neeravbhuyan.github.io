1a505bd8a9b919720987412f6143ef06f9940302
Neeravvv <neeravbhuyan@gmail.com>
2026-04-28 09:51:36 +0530

    Fix Languages component on portfolio site

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import Languages from "./components/Languages";
 import Skills from "./components/Skills";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
